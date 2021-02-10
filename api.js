a80ceabc0a7a9aa42e4cfb06c7dc623031082699
aliahmedkhaled <72753668+aliahmedkhaled@users.noreply.github.com>
2021-02-10 21:57:37 +0100

    termin

--- api.js
+++ api.js
@@ -1,24 +1,29 @@
-/* fetch("https://www.breakingbadapi.com/api/characters").then(function(response){
-return response.json();
-})
-.then(function(data){
-    console.log(data)
-})
-*/
+
+const api="https://www.breakingbadapi.com/api/characters"
+const act=document.querySelector("#name")
+
 async function get(){
-    const response = await fetch("https://www.breakingbadapi.com/api/characters/1")
+    const response = await fetch("https://www.breakingbadapi.com/api/characters")
     const data = await response.json()
     console.log(data)
 
-    
-    
-    document.querySelector("#reponce h1").innerHTML=data[0].name 
-    document.querySelector("#reponce img").src= data[0].img
-    document.querySelector("#reponce h2").innerHTML= data[0].birthday
 
-    document.querySelector("#acteur").innerHTML=
-    `<select>${data.map(actor => '<option>${actor.name}</option>')}
-
-    </select>`
+    acteur.innerHTML+=`<select class="form-control" onchange="getCh(this.value)">
+            <option>Les Acteurs Breaking Bad</option>
+    
+    ${data.map(actor=>`<option>${actor.name}</option>`)}
+        </select>`
+   
 }
+
+async function getCh(name){ 
+    const response = await fetch(`${api}?name=${name}`)
+    const data = await response.json()
+   act.innerHTML=` <h1>${data.name} (${data[0].nickname})</h1>
+   <img src="${data[0].img}"width="250"
+   <h1>${data[0].portrayed}</h1>
+   `
+}
+    
+
 get()
